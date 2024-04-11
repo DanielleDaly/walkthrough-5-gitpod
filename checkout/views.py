63cf79729a1d93478ae23847c0afaf8423117e30
DanielleDaly <daniellerdaly@gmail.com>
2024-04-11 08:45:23 +0000

    Add stripe card to checkout

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,9 @@ def checkout(request):
     order_form = Orderform()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51P4Imb1667nhNLM4ErmG81qbioe871N8A6cKAHH7Jo2CXTXKjHk0RgJFR3T9vn4juJZmEQkKznUzndvjKSDuhn0i00tXKXZYuW',
+        'client_secret': 'test secret',
     }
 
     return render(request, template, context)
